76fd66d07438301099d392197c56e7fc97b013dc
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-17 17:26:07 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -1,44 +1,73 @@
 import "./style.css";
 
 const projForm = document.querySelector(".proj-form");
-const projName = document.querySelector("#proj-name");
-const projSubmit = document.querySelector(".proj-add");
+projForm.addEventListener("submit", submitProj);
 const projCancel = document.querySelector(".proj-cancel");
 projCancel.addEventListener("click", () => {
-    projForm.classList.add("hide");
-    addProjBtn.classList.remove("hide");
-})
-const projUL = document.querySelector(".proj-ul");
+  projForm.classList.add("hide");
+  addProjBtn.classList.remove("hide");
+});
 const addProjBtn = document.querySelector(".addproj");
 addProjBtn.addEventListener("click", () => {
-    projForm.classList.remove("hide");
-    addProjBtn.classList.add("hide");
-})
+  projForm.classList.remove("hide");
+  addProjBtn.classList.add("hide");
+});
 
 const listForm = document.querySelector(".list-form");
 const listName = document.querySelector("#list-name");
 const listSubmit = document.querySelector(".list-add");
 const listCancel = document.querySelector(".list-cancel");
 listCancel.addEventListener("click", () => {
-    listForm.classList.add("hide");
-    addListBtn.classList.remove("hide")
-})
+  listForm.classList.add("hide");
+  addListBtn.classList.remove("hide");
+});
 const listUL = document.querySelector(".list-ul");
 const addListBtn = document.querySelector(".addlist");
 addListBtn.addEventListener("click", () => {
-    listForm.classList.remove("hide");
-    addListBtn.classList.add("hide");
-})
+  listForm.classList.remove("hide");
+  addListBtn.classList.add("hide");
+});
 
 const taskForm = document.querySelector(".task-form");
 const addTaskBtn = document.querySelector(".addtask");
 addTaskBtn.addEventListener("click", () => {
-    taskForm.classList.remove("hide");
-    addTaskBtn.classList.add("hide");
-})
+  taskForm.classList.remove("hide");
+  addTaskBtn.classList.add("hide");
+});
 const taskCancel = document.querySelector(".task-cancel");
 taskCancel.addEventListener("click", () => {
-    taskForm.classList.add("hide");
-    addTaskBtn.classList.remove("hide");
-})
+  taskForm.classList.add("hide");
+  addTaskBtn.classList.remove("hide");
+});
 
+// app logic
+let projList = [];
+if (localStorage.getItem("projList")) {
+  projList = JSON.parse(localStorage.getItem("projList"));
+}
+
+function addNewProj(proj) {
+  projList.push(proj);
+  displayProj();
+  saveProj();
+}
+
+function saveProj() {
+  localStorage.setItem("projList", JSON.stringify(projList));
+}
+
+function displayProj() {
+  const projUL = document.querySelector(".proj-ul");
+  for (const proj of projList) {
+    const projItems = document.createElement("li");
+    projItems.textContent = proj;
+    projUL.appendChild(projItems);
+  }
+}
+
+function submitProj(event) {
+    const projName = document.querySelector("#proj-name");
+    event.preventDefault();
+    const proj = projName.value.trim();
+    addNewProj(proj);
+}
