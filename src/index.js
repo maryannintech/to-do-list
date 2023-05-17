6b08fe5c66c4e0d7395de4b7030bedb7857b5704
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-17 16:12:38 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
+const projForm = document.querySelector(".proj-form");
+const projName = document.querySelector("#proj-name");
+const projSubmit = document.querySelector(".proj-add");
+const projCancel = document.querySelector(".proj-cancel");
+
+const listForm = document.querySelector(".list-form");
+const listName = document.querySelector("#list-name");
+const listSubmit = document.querySelector(".list-add");
+const listCancel = document.querySelector(".list-cancel");
+
+
+function addProject (name) {
+    name = projName.value;
+}
