11e2c22646bad1c6b2f6289a8a781fcb0d57ecd0
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-17 16:37:51 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import _ from "lodash";
 import "./style.css";
 
 const projForm = document.querySelector(".proj-form");
 const projName = document.querySelector("#proj-name");
 const projSubmit = document.querySelector(".proj-add");
 const projCancel = document.querySelector(".proj-cancel");
+projCancel.addEventListener("click", () => {
+    projForm.classList.add("hide");
+    addProjBtn.classList.remove("hide");
+})
 const projUL = document.querySelector(".proj-ul");
 const addProjBtn = document.querySelector(".addproj");
+addProjBtn.addEventListener("click", () => {
+    projForm.classList.remove("hide");
+    addProjBtn.classList.add("hide");
+})
 
 const listForm = document.querySelector(".list-form");
 const listName = document.querySelector("#list-name");
 const listSubmit = document.querySelector(".list-add");
 const listCancel = document.querySelector(".list-cancel");
-
-
-function addProject (name) {
-    name = projName.value;
-}
+listCancel.addEventListener("click", () => {
+    listForm.classList.add("hide");
+    addListBtn.classList.remove("hide")
+})
+const listUL = document.querySelector(".list-ul");
+const addListBtn = document.querySelector(".addlist");
+addListBtn.addEventListener("click", () => {
+    listForm.classList.remove("hide");
+    addListBtn.classList.add("hide");
+})
