dc3c6dce9dd509535a3842b5f05d7c467f0b13f2
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-25 15:39:46 +0800

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -15,4 +16,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new FaviconsWebpackPlugin({
+      logo: './src/favicon.ico.png',
+    }),
+  ],
 };
