eb8f85b7f4dac2216d3ce2be69c9ce42f8d44785
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-26 11:38:45 +0800

    changes

---
+++ src/modules/toggle-mode.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export function initToggle() {
+  const modeToggle = document.querySelector(".toggle");
+  const headerTagline = document.querySelector(".tagline");
+  const bodyElement = document.querySelector("body");
+  const mainElement = document.querySelector("main");
+  modeToggle.addEventListener("click", () => {
+    mainElement.classList.toggle("dark-mode");
+    bodyElement.classList.toggle("dark-mode");
+    headerTagline.classList.toggle("dark-mode");
+  });
+}
